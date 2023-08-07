96d910a62bbea86a9a0c44921197272835d8a969
ChukwuEmeka1269 <emekaukwuoma546@gmail.com>
2023-08-07 16:10:31 +0100

    Implementation for map based service. Closes #6

---
+++ pet-clinic-data/src/main/java/com/js9/js9petclinic/service/mapBasedServiceImpl/AbstractMapService.java
@@ -0,0 +1,39 @@
+package com.js9.js9petclinic.service.mapBasedServiceImpl;
+
+import com.js9.js9petclinic.service.CrudService;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public abstract class AbstractMapService<T, ID> implements CrudService<T,ID> {
+    protected Map<ID, T> map = new HashMap<>();
+
+
+    public Set<T> findAll(){
+        return new HashSet<>(map.values());
+    }
+
+    public T findById(ID id){
+        return map.get(id);
+    }
+
+    public T save(ID id, T object){
+        map.put(id, object);
+
+        return object;
+    }
+
+    public void deleteById(ID id){
+        map.remove(id);
+    }
+
+    public void delete(T object){
+        map.entrySet().removeIf(entry -> entry.getValue().equals(object));
+    }
+
+
+
+
+}

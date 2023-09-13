42151391a9ad5ce91d3ccb133f302416de63db4c
ChukwuEmeka1269 <emekaukwuoma546@gmail.com>
2023-09-14 00:13:31 +0100

    Created Specialty entity and associated with Vet . Closes #28

---
+++ pet-clinic-data/src/main/java/com/js9/js9petclinic/model/Specialty.java
@@ -0,0 +1,10 @@
+package com.js9.js9petclinic.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Specialty extends BaseEntity{
+    private String description;
+}

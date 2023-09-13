9262e08aa8f35af1310e396d69415f9cd07d2e94
ChukwuEmeka1269 <emekaukwuoma546@gmail.com>
2023-09-14 00:55:15 +0100

    Added PetType map service implementation. Closes #30

--- pet-clinic-data/src/main/java/com/js9/js9petclinic/service/PetService.java
+++ pet-clinic-data/src/main/java/com/js9/js9petclinic/service/PetService.java
@@ -1,9 +1,9 @@
 package com.js9.js9petclinic.service;
 
 import com.js9.js9petclinic.model.Pet;
+import com.js9.js9petclinic.model.PetType;
 
 
-
-public interface PetService extends CrudService<Pet, Long>{
+public interface PetService extends CrudService<PetType, Long>{
 
 }
---
+++ pet-clinic-data/src/main/java/com/js9/js9petclinic/service/PetTypeService.java
@@ -0,0 +1,6 @@
+package com.js9.js9petclinic.service;
+
+import com.js9.js9petclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
